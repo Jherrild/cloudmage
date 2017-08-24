6a329ff8f8269d8bb0d635cd162edf2c75559731
Jesten Herrild <jherrild@expedia.com>
2017-08-24 12:38:18 -0700

    Trying to fix the server exception in the example app

--- backend/src/main/kotlin/sampleServer.kt
+++ backend/src/main/kotlin/sampleServer.kt
@@ -1,36 +1,19 @@
-import com.google.gson.Gson
-import org.jetbrains.ktor.application.call
-import org.jetbrains.ktor.host.embeddedServer
-import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.response.header
-import org.jetbrains.ktor.response.respondText
-import org.jetbrains.ktor.routing.get
-import org.jetbrains.ktor.routing.routing
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.response.*
 
 /**
  * @author jestenh@gmail.com
  * Created on 8/22/17
  */
-
 fun main(args: Array<String>) {
-    val gson = Gson()
-
     embeddedServer(Netty, 8080) {
         routing {
-            //TODO: Figure out why get("/path") isn't working
-            get("/api/ping/{count?}") {
-                var count: Int = Integer.valueOf(call.parameters["count"] ?: "1")
-
-                if (count < 1) {
-                    count = 1
-                }
-
-                var obj = Array<Entry>(count, { i -> Entry("$i: Hello, World!") })
-                var str = gson.toJson(obj)
-
-                call.response.header("Access-Control-Allow-Origin", "*")
-                call.respondText(str, ContentType.Application.Json)
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
